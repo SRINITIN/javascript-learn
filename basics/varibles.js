f34d2b5bd8274896f1041a0ab2f64d12f25f1a4d
Nitin Kumar Srivastava <99958656+SRINITIN@users.noreply.github.com>
2023-10-22 13:51:54 +0000

    variables

---
+++ basics/varibles.js
@@ -0,0 +1,20 @@
+const accountId = 123456
+let accountEmail = "nitin@gmail.com"
+var accountPassword = "789456"
+accountCity = "Lucknow"
+let accountState;
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// accountId = 7890123
+accountEmail = "ritika@gmail.com"
+accountPassword = "456"
+accountCity = "Bengaluru"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
