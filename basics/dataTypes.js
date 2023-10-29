fcc89db7676911d6a2ab7da9a1dbfcc4242de7f0
Nitin Kumar Srivastava <99958656+SRINITIN@users.noreply.github.com>
2023-10-29 19:27:38 +0000

    datatypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let name = "ritika"
+let age = 18
+let isLogged = true
+
+console.log(typeof name)
+console.log(typeof age);
+console.log(typeof isLogged);
+console.log(typeof null);
+console.log(typeof undefined);
